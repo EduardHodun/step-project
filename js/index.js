103cc0156f29bcad451c154b545288106a56bef3
Eduard Hodun <eduard.hodun@gmail.com>
2023-06-05 16:58:51 +0300

    adds changes to the js file - tubs

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 const ourServicesBtn = document.querySelector('.service-list-btns');
 console.log(ourServicesBtn);
 
-const ourServicesAllButtons = document.querySelectorAll(".service-list-btn")
+const ourServicesAllButtons = document.querySelectorAll(".service-list-btn");
 console.log(ourServicesAllButtons);
+
+const serviceListDescription = document.querySelectorAll('.service-description-js');
+console.log(serviceListDescription);
 
 ourServicesBtn.addEventListener("click", (e) => {
 
+    e.preventDefault();
 
     for (let i = 0; i < ourServicesAllButtons.length; i++) {
         
@@ -15,5 +19,18 @@ ourServicesBtn.addEventListener("click", (e) => {
     }
 
     e.target.classList.add('service-btn-active');
+
+    for (let i = 0; i < serviceListDescription.length; i++) {
+
+        console.log(serviceListDescription[i]);
+        serviceListDescription[i].style.display = "none";
+
+        if (serviceListDescription[i].dataset.name === e.target.dataset.name) {
+            serviceListDescription[i].style.display = "block";
+             }
+    }
+
+    
+
     
 })
